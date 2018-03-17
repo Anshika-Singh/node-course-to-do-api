949e222567c8c1d66ebb6a15c35cf0759f85157d
Anshika Singh <anshika135singh@gmail.com>
2018-03-17 18:48:08 -0500

    User password save

--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const validator = require('validator');
 const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 
 var UserSchema = new mongoose.Schema({
 	email:{
@@ -65,6 +66,24 @@ UserSchema.statics.findByToken = function (token) {
 
 	return User.findOne({'_id': decoded._id, 'tokens.token' : token, 'tokens.access':'auth'});
 };
+
+
+
+UserSchema.pre('save', function(next) {
+	var user = this;
+
+	if(user.isModified('password')){
+		bcrypt.genSalt(10, (err, salt) => {
+			bcrypt.hash(user.password, salt, (err, hash)=> {
+				user.password = hash;
+				console.log(hash);
+				next();
+			});
+		});
+	} else {
+		next();
+	}
+})
 
 var User = mongoose.model('User', UserSchema);
 
